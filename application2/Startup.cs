e36aa992f93a7fecbb5f49b76e7bf465423fc9db
sravanvsk <vorugantisravan7@gmail.com>
2021-11-17 12:41:26 +0530

    Modified

--- application2/Startup.cs
+++ application2/Startup.cs
@@ -32,7 +32,7 @@ namespace application2
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!");sa,mf,mdclklhkhkjkj\\  [prop-kpojkp[]]
 
                 });
             });
